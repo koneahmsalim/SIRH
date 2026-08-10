3e023ba82c0a5919550d3f889f00afdc900038ab
kbrah <smurfingvalora@gmail.com>
2026-08-10 12:54:13 +0000

    Adapter le pipeline de recrutement aux 10 etapes du process RH Villa Nova

---
+++ addons/villa_nova_recruitment/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Villa Nova - Processus de Recrutement",
+    'summary': "Adapte le pipeline de recrutement Odoo a la procedure RH 2025 de Villa Nova / Infinity Africa Group",
+    'version': '18.0.1.0.0',
+    'category': 'Human Resources/Recruitment',
+    'author': 'Villa Nova',
+    'license': 'LGPL-3',
+    'depends': ['hr_recruitment'],
+    'data': [
+        'data/recruitment_stage_data.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
