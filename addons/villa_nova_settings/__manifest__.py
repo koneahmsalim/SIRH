46451c1b7c4af6f7d7073b11e5516351a926714f
kbrah <smurfingvalora@gmail.com>
2026-08-13 10:12:57 +0000

    Nouveau module villa_nova_settings : case a cocher 'Compte actif' + bandeau Archive directement visibles sur la fiche Reglages > Utilisateurs (au lieu du menu Actions cache de la liste)

---
+++ addons/villa_nova_settings/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Villa Nova - Réglages",
+    'summary': "Ajustements transverses de l'interface Reglages (ex: desactivation d'utilisateurs "
+               "visible directement sur la fiche, plutot que caché dans le menu Actions de la liste)",
+    'version': '18.0.1.0.0',
+    'category': 'Human Resources',
+    'author': 'Villa Nova',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'views/res_users_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
